17d957f7b5c3e5e78542e7e10b136f68ded37548
Ritvik-exe <ritvikyalala@gmail.com>
2026-06-27 12:22:34 +0100

    Add: Blocking public access and cors for S3

--- Infra/main.tf
+++ Infra/main.tf
@@ -38,6 +38,25 @@ resource "aws_s3_bucket_versioning" "invoice_version"{
     }
 }
 
+resource "aws_s3_bucket_public_access_block" "invoice_public_block"{
+    bucket = aws_s3_bucket.invoice.id
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_cors_configuration" "invoice_cors"{
+    bucket = aws_s3_bucket.invoice.id
+    cors_rule {
+        allowed_headers = ["*"]
+        allowed_methods = ["GET", "PUT", "POST", "HEAD"]
+        allowed_origins = ["https://busynes.com", "http://localhost:5173", "http://localhost:8080", "http://localhost:3000"]
+        expose_headers = ["ETag"]
+        max_age_seconds = 300
+    }
+}
+
 # ========================== DynamoDB ========================== 
 resource "aws_dynamodb_table" "memory"{
     name = var.busynes_db
